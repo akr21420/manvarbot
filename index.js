7d7b42c270c88bbede48c1559b553e60563e8a28
Ananthakrishna <akrrao7@gmail.com>
2021-06-07 14:05:15 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+const fs = require('fs')
+const pre = '-';
+client.commands = new discord.Collection();
+const cmdfiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of cmdfiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name,command)
+}
+client.once('ready',()=>{
+    console.log("Hello");
+})
+client.on('message',message=>{
+    if(!message.content.startsWith(pre) || message.author.bot)return;
+    const args = message.content.slice(pre.length).split(/ +/);
+    const cmd = args.shift().toLowerCase();
+    console.log(message.content);
+    if(message.content.length == 5)
+    message.channel.send("Enta maraya message haaka");
+    else{
+    if(cmd === 'ping'){
+        client.commands.get('itslike').execute(message,args);
+    }
+}
+})
+client.login('ODUxMzYxNzc2NzI0NjA3MDM2.YL3KcQ.USTbIxZqYJOp6F6s6lt0Qe3IQrw')
